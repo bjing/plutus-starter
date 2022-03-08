b4e03ccdae206ea40523ffc2fe21c979b2be7fcd
Brian Jing <brianjing404@gmail.com>
2022-03-08 14:48:33 +1100

    Remoove strict Plutus Core result script checking

--- examples/test/Spec/Game.hs
+++ examples/test/Spec/Game.hs
@@ -1,30 +1,30 @@
-{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE NumericUnderscores #-}
-{-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE TemplateHaskell    #-}
-{-# LANGUAGE TypeApplications   #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Spec.Game
-    ( tests
-    ) where
+  ( tests
+  ) where
 
-import           Control.Monad         (void)
-import           Ledger                (ValidationError (ScriptFailure))
-import qualified Ledger.Ada            as Ada
-import           Plutus.Contract       (Contract)
-import           Plutus.Contract.Test
-import           Plutus.Contract.Types (ContractError (WalletContractError))
-import           Plutus.Contracts.Game
-import           Plutus.Trace.Emulator (ContractInstanceTag)
+import Control.Monad (void)
+import Ledger (ValidationError(ScriptFailure))
+import qualified Ledger.Ada as Ada
+import Plutus.Contract (Contract)
+import Plutus.Contract.Test
+import Plutus.Contract.Types (ContractError(WalletContractError))
+import Plutus.Contracts.Game
+import Plutus.Trace.Emulator (ContractInstanceTag)
 import qualified Plutus.Trace.Emulator as Trace
-import qualified PlutusTx
-import           Prelude
-import           Test.Tasty
-import qualified Test.Tasty.HUnit      as HUnit
-import           Wallet.API            (WalletAPIError (ValidationError))
+import Prelude
+import Test.Tasty
+import qualified Test.Tasty.HUnit as HUnit
+import Wallet.API (WalletAPIError(ValidationError))
 
 t1, t2 :: ContractInstanceTag
 t1 = Trace.walletInstanceTag w1
+
 t2 = Trace.walletInstanceTag w2
 
 theContract :: Contract () GameSchema ContractError ()
@@ -33,45 +33,49 @@ theContract = game
 -- W1 locks funds, W2 (and other wallets) should have access to guess endpoint
 -- No funds locked, so W2 (and other wallets) should not have access to guess endpoint
 tests :: TestTree
-tests = testGroup "game"
-    [ checkPredicate "Expose 'lock' endpoint, and 'guess' endpoint"
-        (endpointAvailable @"lock" theContract t1
-          .&&. (endpointAvailable @"guess" theContract t1))
-        $ void $ Trace.activateContractWallet w1 (lock @ContractError)
-
-    , checkPredicate "'lock' endpoint submits a transaction"
-        (anyTx theContract t1)
-        $ do
-            hdl <- Trace.activateContractWallet w1 theContract
-            Trace.callEndpoint @"lock" hdl (LockParams "secret" (Ada.adaValueOf 10))
-
-    , checkPredicate "'guess' endpoint is available after locking funds"
-        (endpointAvailable @"guess" theContract t2)
-        $ do
-          void $ Trace.activateContractWallet w2 theContract
-          lockTrace w1 "secret"
-
-    , checkPredicate "guess right (unlock funds)"
-        (walletFundsChange w2 (Ada.adaValueOf 10)
-          .&&. walletFundsChange w1 (Ada.adaValueOf (-10)))
-        $ do
-          lockTrace w1 "secret"
-          guessTrace w2 "secret"
-
-    , checkPredicate "guess wrong"
-        (walletFundsChange w2 (Ada.lovelaceValueOf 0)
-          .&&. walletFundsChange w1 (Ada.adaValueOf (-10))
-          .&&. assertContractError guess t2 appropriateError "error should be: WalletError (ValidationError (ScriptFailure _)) ")
-        $ do
-          lockTrace w1 "secret"
-          guessTrace w2 "SECRET"
-
-    , goldenPir "examples/test/Spec/game.pir" $$(PlutusTx.compile [|| validateGuess ||])
-
-    , HUnit.testCase "script size is reasonable" (reasonable gameValidator 20000)
+tests =
+  testGroup
+    "game"
+    [ checkPredicate
+        "Expose 'lock' endpoint, and 'guess' endpoint"
+        (endpointAvailable @"lock" theContract t1 .&&.
+         (endpointAvailable @"guess" theContract t1)) $
+      void $ Trace.activateContractWallet w1 (lock @ContractError)
+    , checkPredicate
+        "'lock' endpoint submits a transaction"
+        (anyTx theContract t1) $ do
+        hdl <- Trace.activateContractWallet w1 theContract
+        Trace.callEndpoint @"lock" hdl (LockParams "secret" (Ada.adaValueOf 10))
+    , checkPredicate
+        "'guess' endpoint is available after locking funds"
+        (endpointAvailable @"guess" theContract t2) $ do
+        void $ Trace.activateContractWallet w2 theContract
+        lockTrace w1 "secret"
+    , checkPredicate
+        "guess right (unlock funds)"
+        (walletFundsChange w2 (Ada.adaValueOf 10) .&&.
+         walletFundsChange w1 (Ada.adaValueOf (-10))) $ do
+        lockTrace w1 "secret"
+        guessTrace w2 "secret"
+    , checkPredicate
+        "guess wrong"
+        (walletFundsChange w2 (Ada.lovelaceValueOf 0) .&&.
+         walletFundsChange w1 (Ada.adaValueOf (-10)) .&&.
+         assertContractError
+           guess
+           t2
+           appropriateError
+           "error should be: WalletError (ValidationError (ScriptFailure _)) ") $ do
+        lockTrace w1 "secret"
+        guessTrace w2 "SECRET"
+    --, goldenPir "examples/test/Spec/game.pir" $$(PlutusTx.compile [|| validateGuess ||])
+    , HUnit.testCase
+        "script size is reasonable"
+        (reasonable gameValidator 20000)
     ]
 
 appropriateError :: ContractError -> Bool
-appropriateError e = case e of
+appropriateError e =
+  case e of
     WalletContractError (ValidationError (ScriptFailure _)) -> True
-    _                                                       -> False
+    _ -> False
